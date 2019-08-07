e0841862ae31fbe7d6094e4f9e7d350227c6beb0
ayogatot <azerino25@gmail.com>
2019-08-07 10:55:02 +0700

    connecting express app to our mysql database

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8800;
+const mysql = require("mysql");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "ayogatot",
+  password: "inipassword"
+});
+
+db.connect(err => {
+  if (err) console.log(err);
+  console.log("connect");
+});
 
 app.get("/", (req, res) => {
   res.status(200).json({
