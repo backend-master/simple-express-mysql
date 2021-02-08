467a963034d05fc9994130d9d254fb67cfd03e9f
ayogatot <azerino25@gmail.com>
2021-02-08 13:19:49 +0700

    Deploying to docker

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8800;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const PORT = 5000;
+
+const db = require('./config/db');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const bookRoute = require("./routes/books");
+const bookRoute = require('./routes/books');
 
-app.use("/api/v1/books", bookRoute);
+app.use('/api/v1/books', bookRoute);
+app.use('/test', (req, res) => {
+  db.query('SELECT 1', [], (err, result) => {
+    if (err) {
+      res.status(500).json({ err });
+    } else {
+      res.status(200).json({ msg: 'connected' });
+    }
+  });
+});
 
 app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}`)
--- config/db.js
+++ config/db.js
@@ -1,12 +1,22 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const mysql = require("mysql");
+const mysql = require('mysql');
+
+console.log(process.env);
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB,
+  port: process.env.MYSQL_PORT,
+});
+
+db.connect((err) => {
+  if (err) console.log(`Error ${err}`);
+  else {
+    console.log('running');
+  }
 });
 
 module.exports = db;
